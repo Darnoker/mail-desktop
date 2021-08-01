93cc033a9b38e84bb306f4e996ec34b95fdb8242
Darnoker <73139334+Darnoker@users.noreply.github.com>
2021-08-01 23:07:51 +0200

    fix modules path

--- view/viewHandler.py
+++ view/viewHandler.py
@@ -1,18 +1,13 @@
-from loginWindow import Ui_loginWindow
+from view.loginWindow import Ui_loginWindow
 from PyQt5 import QtWidgets
+import sys
 
 
 class ViewHandler:
     def showLoginWindow(self):
-        import sys
         app = QtWidgets.QApplication(sys.argv)
         loginWindow = QtWidgets.QDialog()
-        ui=Ui_loginWindow()
+        ui = Ui_loginWindow()
         ui.setupUi(loginWindow)
         loginWindow.show()
         sys.exit(app.exec_())
-        
-
-
-
-
