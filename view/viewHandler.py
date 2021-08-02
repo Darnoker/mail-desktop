49d51413f9d3366b76d05a3b3a5590003f1ee46b
Darnoker <73139334+Darnoker@users.noreply.github.com>
2021-08-02 17:01:01 +0200

    Update viewHandler

--- view/viewHandler.py
+++ view/viewHandler.py
@@ -1,3 +1,4 @@
+from view.mainWindow import Ui_MainWindow
 from view.loginWindow import Ui_loginWindow
 from PyQt5 import QtWidgets
 import sys
@@ -10,4 +11,12 @@ class ViewHandler:
         ui = Ui_loginWindow()
         ui.setupUi(loginWindow)
         loginWindow.show()
+        sys.exit(app.exec_())
+    
+    def showMainWindow(self):
+        app = QtWidgets.QApplication(sys.argv)
+        mainWindow = QtWidgets.QMainWindow()
+        ui = Ui_MainWindow()
+        ui.setupUi(mainWindow)
+        mainWindow.show()
         sys.exit(app.exec_())
