5306e1e4efd0acde4a25124b1761ca26fc989f4e
Darnoker <73139334+Darnoker@users.noreply.github.com>
2021-08-07 18:30:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from view.viewHandler import ViewHandler
+import sys
 
+from PyQt5 import QtWidgets
+
+from models.EmailAccount import EmailAccount
+from services.EmailService import EmailService
+from services.LoginService import LoginService
+from view.ViewHandler import ViewHandler
+
+# declare variable app.
+app = QtWidgets.QApplication(sys.argv)
+
+# declare viewHandler to manage our view.
 viewHandler = ViewHandler()
 viewHandler.showLoginWindow()
+
+# exit app
+sys.exit(app.exec_())
