6d1abc3c81d85ebd9d55387369f84b2b2f505880
Darnoker <73139334+Darnoker@users.noreply.github.com>
2021-08-08 16:42:07 +0200

    add logic to LoginWindowController.py and update LoginService.py

--- controllers/LoginWindowController.py
+++ controllers/LoginWindowController.py
@@ -1,11 +1,24 @@
+from models.EmailAccount import EmailAccount
+from services.EmailService import EmailService
+from services.LoginService import LoginService
+
+
 class LoginWindowController:
     def __init__(self, loginWindow):
         self.loginWindow = loginWindow
+        self.emailAccount = None
+        self.loginService = None
 
     def loginButtonAction(self):
         if self.checkFields():
-            self.loginWindow.viewHandler.showMainWindow()
-            self.loginWindow.viewHandler.closeLoginWindow()
+            self.emailAccount = EmailAccount(self.loginWindow.emailAdressField.text(),
+                                             self.loginWindow.passwordField.text())
+
+            self.loginService = LoginService(self.emailAccount, self.loginWindow)
+
+            if self.loginService.login_() and self.checkFields():
+                self.loginWindow.viewHandler.showMainWindow()
+                self.loginWindow.viewHandler.closeLoginWindow()
 
     def checkFields(self):
         if not self.loginWindow.emailAdressField.text():
@@ -14,5 +27,4 @@ class LoginWindowController:
         if not self.loginWindow.passwordField.text():
             self.loginWindow.errorLabel.setText("Password field is empty!")
             return False
-
         return True
--- services/LoginService.py
+++ services/LoginService.py
@@ -2,12 +2,16 @@ import traceback
 
 
 class LoginService:
-    def __init__(self, emailAccount):
+    def __init__(self, emailAccount, loginWindow):
         self.emailAccount = emailAccount
+        self.loginWindow = loginWindow
 
     def login_(self):
         try:
             self.emailAccount.mail.login(self.emailAccount.address, self.emailAccount.password)
+            return True
         except Exception as e:
             traceback.print_exc()
             print(str(e))
+            self.loginWindow.errorLabel.setText("Invalid username or password!")
+            return False
