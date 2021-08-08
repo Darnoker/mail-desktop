80aea58476ab8b697639b91053160eb41d87497a
Darnoker <73139334+Darnoker@users.noreply.github.com>
2021-08-08 16:44:44 +0200

    Update login service and controller (#8)
    * add logic to LoginWindowController.py and update LoginService.py
    
    * remove EmailService from imports

--- controllers/LoginWindowController.py
+++ controllers/LoginWindowController.py
@@ -1,18 +1,31 @@
+from models.EmailAccount import EmailAccount
+from services.LoginService import LoginService
+
+
 class LoginWindowController:
     def __init__(self, loginWindow):
         self.loginWindow = loginWindow
+        self.emailAccount = None
+        self.loginService = None
 
     def loginButtonAction(self):
         if self.checkFields():
-            self.loginWindow.viewHandler.showMainWindow()
-            self.loginWindow.viewHandler.closeLoginWindow()
+            self.emailAccount = EmailAccount(self.loginWindow.emailAdressField.text(),
+                                             self.loginWindow.passwordField.text())
+            
+            self.loginService = LoginService(self.emailAccount, self.loginWindow)
+
+            if self.loginService.login_() and self.checkFields():
+                self.loginWindow.viewHandler.showMainWindow()
+                self.loginWindow.viewHandler.closeLoginWindow()
 
     def checkFields(self):
         if not self.loginWindow.emailAdressField.text():
             self.loginWindow.errorLabel.setText("Email Address field is empty!")
             return False
+        
         if not self.loginWindow.passwordField.text():
             self.loginWindow.errorLabel.setText("Password field is empty!")
             return False
-
+        
         return True
